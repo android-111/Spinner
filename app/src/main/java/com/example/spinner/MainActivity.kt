9f43e84a3dad099121e98ae53f75e69e5f90ab08
raja <raza404y@gmail.com>
2024-07-01 06:50:01 +0500

    initial commit

---
+++ app/src/main/java/com/example/spinner/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.spinner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.spinner.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+        val arrayLanguage = arrayOf("Select Language", "Spanish", "English", "French", "German")
+        val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,arrayLanguage)
+        binding.spinner.adapter = adapter
+        
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = arrayLanguage[position]
+                if (!selectedItem.equals("Select Language")){
+                    Toast.makeText(this@MainActivity, "Selected $selectedItem", Toast.LENGTH_SHORT).show()
+                }
+            
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+    }
+}
